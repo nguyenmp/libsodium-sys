23ef1d0bad3a9d774c60a6d10ee6fa4945a972fd
Mark Nguyen <mnguyen4@apple.com>
2018-08-28 03:04:35 -0700

    Implemented bridges for random, init, hashing, and bin2hex

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate libc;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -12,4 +14,132 @@ fn bar() -> &'static str {
 
 pub fn foo() -> &'static str {
     bar()
+}
+
+pub mod helpers {
+	use libc::c_char;
+	use libc::c_uchar;
+	use libc::size_t;
+	use std::ffi::CString;
+
+	#[link(name = "sodium")]
+	extern {
+		fn sodium_bin2hex(hex: *const c_char, hex_maxlen: size_t,
+			bin: *const c_uchar, bin_len: size_t) -> *mut c_char;
+	}
+
+	pub fn bin_to_hex(bin: &[u8]) -> String {
+		// Buffer must be at least bin_len * 2 + 1
+		let hex_max_len = bin.len() * 2 + 1;
+		let hex = CString::new(vec!('0' as u8, hex_max_len as u8)).unwrap();
+
+		// The function always returns hex, but we already have hex
+		unsafe {
+			let hex_raw = sodium_bin2hex(hex.into_raw(), hex_max_len,
+				bin.as_ptr(), bin.len());
+			CString::from_raw(hex_raw).into_string().unwrap()
+		}
+	}
+}
+
+pub mod hash {
+	use libc::size_t;
+	use libc::c_ulonglong;
+	use libc::c_uchar;
+	use libc::c_int;
+	use std::ptr::null;
+
+	#[link(name = "sodium")]
+	extern {
+		fn crypto_generichash_bytes_max() -> size_t;
+		fn crypto_generichash(output: *mut c_uchar, output_length: size_t,
+			input: *const c_uchar, input_length: c_ulonglong,
+			key: *const c_uchar, key_length: size_t) -> c_int;
+	}
+
+	pub fn generic(input: &[u8], key: Option<&[u8]>) -> Vec<u8> {
+		super::init();
+		let output_length = unsafe {
+			crypto_generichash_bytes_max()
+		};
+		let key_ptr = match key {
+			Some(k) => k.as_ptr(),
+			None => null(),
+		};
+		let key_len = match key {
+			Some(k) => k.len(),
+			None => 0,
+		};
+		let mut output = vec!(0; output_length);
+		let result = unsafe {
+			crypto_generichash(output.as_mut_ptr(), output.len(),
+				input.as_ptr(), input.len() as c_ulonglong,
+				key_ptr, key_len)
+		};
+		output
+	}
+
+}
+
+pub mod random {
+	use libc::uint32_t;
+	// use libc::size_t;
+	// use libc::c_void;
+
+	#[link(name = "sodium")]
+	extern {
+		fn randombytes_random() -> uint32_t;
+		fn randombytes_uniform(_: uint32_t) -> uint32_t;
+		// fn randombytes_buf(buf: *const c_void, size: size_t);
+	}
+
+	pub fn int() -> u32 {
+		super::init();
+		unsafe {
+			randombytes_random()
+		}
+	}
+
+	pub fn range(limit: u32) -> u32 {
+		super::init();
+		unsafe {
+			randombytes_uniform(limit)
+		}
+	}
+
+	pub fn buffer(buf: &mut[u8]) {
+		super::init();
+		buf.as_mut_ptr();
+		assert!(false);
+		// unsafe {
+			// randombytes_buf(buf.as_prt(), buf.len())
+		// }
+	}
+}
+
+use libc::c_int;
+
+#[link(name = "sodium")]
+extern {
+	fn sodium_init() -> c_int;
+}
+
+fn init() {
+	let result : c_int = unsafe {
+		sodium_init()
+	};
+
+	assert!(result >= 0);
+}
+
+#[test]
+fn assert_sodium_init_once() {
+	init();
+}
+
+
+#[test]
+fn assert_sodium_init_twice() {
+	init();
+	init();
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,51 @@
 extern crate sodium;
 
+// This test simply verifies that we can call into the sodium module
 #[test]
 fn foo_works_in_integration() {
     assert_eq!("asdff", sodium::foo());
+}
+
+// Many of these tests are applications of the quick start FAQs
+// https://download.libsodium.org/doc/quickstart/
+
+#[test]
+fn random_int() {
+	let _ : u32 = sodium::random::int();
+}
+
+#[test]
+fn random_range() {
+	let _ : u32 = sodium::random::range(32);
+}
+
+#[test]
+fn random_buffer() {
+	let buffer = &mut [0, 0, 0, 0];
+	sodium::random::buffer(buffer);
+}
+
+#[test]
+fn compute_a_hash_and_verify() {
+	let bin = [];
+	let key = None;
+	let hash_bin = sodium::hash::generic(&bin, key);
+	let hash_hex = sodium::helpers::bin_to_hex(&hash_bin[..]);
+	let known_correct_answer = "786A02F742015903C6C6FD852552D272912F4740E1584\
+	7618A86E217F71F5419D25E1031AFEE585313896444934EB04B903A685B1448B755D56F70\
+	1AFE9BE2CE";
+	assert_eq!(known_correct_answer.to_lowercase(), hash_hex);
+}
+
+#[test]
+fn compute_another_hash_and_verify() {
+	let input = String::from("The quick brown fox jumps over the lazy dog");
+	let bin : &[u8] = input.as_bytes();
+	let key = None;
+	let hash_bin = sodium::hash::generic(bin, key);
+	let hash_hex = sodium::helpers::bin_to_hex(&hash_bin[..]);
+	let known_correct_answer = "A8ADD4BDDDFD93E4877D2746E62817B116364A1FA7BC1\
+	48D95090BC7333B3673F82401CF7AA2E4CB1ECD90296E3F14CB5413F8ED77BE73045B1391\
+	4CDCD6A918";
+	assert_eq!(known_correct_answer.to_lowercase(), hash_hex);
 }
